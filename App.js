932828c2d8db9806fbca18b4a0b7449b51ab1a19
bernardkerun <bernard.jiang@thehovercam.com>
2018-08-30 11:08:47 -0700

    Added gun-asyncstorage.

--- App.js
+++ App.js
@@ -8,6 +8,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import 'gun-asyncstorage';
+
 import Gun from 'gun/gun.js';
 
 const gun = new Gun('http://gunjs.herokuapp.com/gun');
